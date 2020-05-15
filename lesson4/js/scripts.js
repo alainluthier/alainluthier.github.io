0433c182602f4d23bb15ba745e1266828f8de832
alainluthier <alainluthier@gmail.com>
2020-05-15 18:27:00 -0400

    final week04

---
+++ lesson4/js/scripts.js
@@ -0,0 +1,11 @@
+
+function toogleButton(){
+    document.getElementById("primaryNav").classList.toggle("hide");
+}
+const currentDate = new Date();
+console.log(currentDate)
+const formatterMonth = new Intl.DateTimeFormat('en', { month: 'long' });
+const formatterWeekday = new Intl.DateTimeFormat('en', { weekday: 'long' });
+const month = formatterMonth.format(currentDate);
+const weekday = formatterWeekday.format(currentDate);
+document.getElementById("currentDate").textContent = weekday+ ", "+currentDate.getDate()+" "+month + " "+currentDate.getFullYear();
