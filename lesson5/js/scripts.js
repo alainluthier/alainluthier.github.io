c4e1086c3123fc86c67408511f92fe918db96b30
alainluthier <alainluthier@gmail.com>
2020-05-22 17:28:32 -0400

    final preston

--- lesson5/js/scripts.js
+++ lesson5/js/scripts.js
@@ -3,10 +3,15 @@ function toogleButton(){
     document.getElementById("primaryNav").classList.toggle("hide");
 }
 const currentDate = new Date();
+console.log(currentDate)
 const formatterMonth = new Intl.DateTimeFormat('en', { month: 'long' });
 const formatterWeekday = new Intl.DateTimeFormat('en', { weekday: 'long' });
 const month = formatterMonth.format(currentDate);
 const weekday = formatterWeekday.format(currentDate);
 document.getElementById("currentDate").textContent = weekday+ ", "+currentDate.getDate()+" "+month + " "+currentDate.getFullYear();
-
-
+console.log(currentDate.getDay())
+if(currentDate.getDay()==6){
+    document.getElementById("message").classList.add("showme");
+}else{
+    document.getElementById("message").classList.add("hideme");
+}
