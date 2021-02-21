ff20a3a387094d79f0a661909c59b761c968b51e
alainluthier <alainluthier@gmail.com>
2021-02-20 21:07:26 -0400

    todo change

--- wdd330/week06/todo/ls.js
+++ wdd330/week06/todo/ls.js
@@ -1,21 +1,9 @@
-const todoList = [{
-        id: 1,
-        content: 'test1',
-        completed: false
-    },
-    {
-        id: 2,
-        content: 'test2',
-        completed: false
-    },
-    {
-        id: 3,
-        content: 'test3',
-        completed: false
-    },
-]
+let todoList = [];
 export default class ls {
     constructor() {
+        if(JSON.parse(localStorage.getItem("todoList")) != null){
+            todoList = JSON.parse(localStorage.getItem("todoList"));
+        }
     }
     getAlltoDoList() {
         return todoList;
@@ -24,11 +12,13 @@ export default class ls {
         let id = Date.now();
         let content = item;
         todoList.push({"id":id,"content":content, "completed":false});
+        localStorage.setItem("todoList", JSON.stringify(todoList));
     }
     removeItem(item){
         let index=todoList.findIndex(x=>x.id==item);
         console.log(index);
         todoList.splice(index,1);
+        localStorage.setItem("todoList", JSON.stringify(todoList));
     }
     toggleItem(item){
         let index=todoList.findIndex(x=>x.id==item);
@@ -37,5 +27,6 @@ export default class ls {
         }else{
             todoList[index].completed=true;
         }
+        localStorage.setItem("todoList", JSON.stringify(todoList));
     }
 }
